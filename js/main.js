d357350b49a033a52526b9e8603596f465c1dbe4
Andres Rios <arrios@live.com>
2022-05-16 00:02:08 -0300

    Corrigiendo los eventos DOM

--- js/main.js
+++ js/main.js
@@ -1,101 +1,99 @@
 let dia = 1
+let LavarPlatosTarea = document.getElementById("lavarPlatos");
+let habitantes = document.getElementById("personas");
+let lavarPlatos = document.getElementById("platos");
+let miembro1 = document.getElementById("divMiembro1");
+let miembro2 = document.getElementById("divMiembro2");
+let miembro3 = document.getElementById("divMiembro3");
+let miembro4 = document.getElementById("divMiembro4");
+let botonNombres = document.getElementById("botonMiembros");
+let nombreMiembro1 = document.getElementById("miembro1");
+let nombreMiembro2 = document.getElementById("miembro2");
+let nombreMiembro3 = document.getElementById("miembro3");
+let nombreMiembro4 = document.getElementById("miembro4");
+let quienHizo = document.getElementById("quienHizo");
+let asignarLasTareas = document.getElementById("asignarTareas");
+const resultadoPlatos = document.getElementById("resultadoPlatos");
+const resultadoMiembro0 = document.getElementById("resultadoMiembro0");
+const resultadoMiembro1 = document.getElementById("resultadoMiembro1");
+const resultadoMiembro2 = document.getElementById("resultadoMiembro2");
+const resultadoMiembro3 = document.getElementById("resultadoMiembro3");
+const resultadoMiembro4 = document.getElementById("resultadoMiembro4");
+const valorTotal = document.getElementById("valorCasa");
 
-const usuarios = [{nombre:"",puntos:0},{nombre:"",puntos:0},{nombre:"",puntos:0},{nombre:"",puntos:0}];
+const usuarios = [{},{},{},{}];
 
-/* const tareas = [{nombre: "Lavar los platos", valor: 0},{nombre: "Limpiar el baño", valor: 0},{nombre: "Cocinar", valor:0},{nombre:"Barrer",valor:0},{nombre:"Hacer la cama",valor:0},{nombre:"Sacar la basura",valor:0}]; */
+const tareas = [{nombre: "Lavar los platos", valor: 0},
+                {nombre: "Limpiar el baño", valor: 0},
+                {nombre: "Cocinar", valor:0},
+                {nombre:"Barrer",valor:0},
+                {nombre:"Hacer la cama",valor:0},
+                {nombre:"Sacar la basura",valor:0}
+            ];
 
-let LavarPlatosTarea = document.getElementById("lavarPlatos").value;
+const cargar = document.getElementById("cargar");
+cargar.addEventListener("click", cargarValores);
 
-const formulario = document.querySelector("#formulario");
-
-formulario.addEventListener("submit", cargarValores);
 
 function cargarValores(evt){
-    /* tareas[0].valor = document.getElementById("lavarPlatos").value;
-    tareas[1].valor = document.getElementById("limpiarBano").value;
-    tareas[2].valor = document.getElementById("cocinar").value;
-    tareas[3].valor = document.getElementById("barrer").value;
-    tareas[4].valor = document.getElementById("hacerCama").value;
-    tareas[5].valor = document.getElementById("basura").value; */
+    let valorPlatos = Number(LavarPlatosTarea.value)
+    tareas[0].valor = valorPlatos;
     evt.preventDefault();
+    res1(valorPlatos);
+    const valorCasa = tareas.reduce((acc,el) => acc+el.valor,0);
+    valorTotal.innerHTML = "Limpiar toda la casa vale "+valorCasa+"pts";
+    switchHabitantes(habitantes.value)
+
+    const ingresarMiembros = document.getElementById("nombres");
+    ingresarMiembros.addEventListener("click", cargarDatos);
+
+    function cargarDatos (evt) {
+        console.log(usuarios)
+        evt.preventDefault()
+        console.log(nombreMiembro1)
+        quienLoHizo()
+        asignarTareas()
+    }
+    
+
+    
 }
 
-const resultado1 = document.getElementById("resultado1")
-/* const resultado2 = document.getElementById("resultado2")
-const resultado3 = document.getElementById("resultado3")
-const resultado4 = document.getElementById("resultado4")
-const resultado5 = document.getElementById("resultado5")
-const resultado6 = document.getElementById("resultado6")
-const resultado7 = document.getElementById("resultado7") */
+function res1(valorPlatos) {
+    if((valorPlatos > 100) || (valorPlatos <= 0)){
+        return resultadoPlatos.innerHTML="Por favor ingrese un valor del 1 al 100";
+    }
+resultadoPlatos.innerHTML=`Lavar los platos ${valorPlatos}pts`;
+}
 
-function res1() {
-    /* if((tareas[0].valor > 100) || (tareas[0].valor <= 0)){ */
-    if((LavarPlatosTarea > 100) || (LavarPlatosTarea <= 0)){
-        return () => resultado1.textContent("Por favor ingrese un valor del 1 al 100");
-    }else{
-        return () => resultado1.textContent(`Lavar los platos ${LavarPlatosTarea}pts`);
-    }
-}
-/* function res2() {
-    if((tareas[1].valor > 100) || (tareas[1].valor <= 0)){
-        return () => resultado2.textContent("Por favor ingrese un valor del 1 al 100");
-    }else{
-        return () => resultado2.textContent(`Limpiar el baño ${tareas[1].valor}pts`);
-    }
-}
-function res3() {
-    if((tareas[2].valor > 100) || (tareas[2].valor <= 0)){
-        return () => resultado3.textContent("Por favor ingrese un valor del 1 al 100");
-    }else{  
-        return () => resultado3.textContent(`Cocinar ${tareas[2].valor}pts`);
-    }
-}
-function res4() {
-    if((tareas[3].valor > 100) || (tareas[3].valor <= 0)){
-        return () => resultado4.textContent("Por favor ingrese un valor del 1 al 100");
-    }else{
-        return () => resultado4.textContent(`Barrer ${tareas[3].valor}pts`);
-    }
-}
-function res5() {
-    if((tareas[4].valor > 100) || (tareas[4].valor <= 0)){
-        return () => resultado5.textContent("Por favor ingrese un valor del 1 al 100");
-    }else{
-        return () => resultado5.textContent(`Hacer la cama ${tareas[4].valor}pts`);
-    }
-}
-function res6() {
-    if((tareas[5].valor > 100) || (tareas[5].valor <= 0)){
-        return () => resultado6.textContent("Por favor ingrese un valor del 1 al 100");
-    }else{
-        return () => resultado6.textContent(`Sacar la basura ${tareas[5].valor}pts`);
-    }
-} */
 
-/* const valorCasa = tareas.reduce((acc,el) => acc+el.valor,0);
-console.log("limpiar toda la casa vale "+valorCasa+"pts"); */
 
-let habitantes = document.getElementById("personas").value;
-switch(habitantes){
+const switchHabitantes = (habitantes) => {
+    switch(habitantes){
     case "1":
-        resultado7.textContent = "No tiene mucho sentido que dividas las tareas";
+        resultadoMiembro0.textContent = "No tiene mucho sentido que dividas las tareas";
         break;
     case "2":
         familiaDeDos()
+        cargarNombres()
         break;
     case "3":
         familiaDeDos()
         familiaDeTres()
+        cargarNombres()
         break;
     case "4":
         familiaDeDos()
         familiaDeTres()
         familiaDeCuatro()
+        cargarNombres()
         break;
     default:
         habitantes.value = "Es mas de lo que podemos manejar";
         break;
+    }
 }
+
 
 function familiaDeDos(){
     const divMiembro1 = document.createElement("div");
@@ -109,46 +107,48 @@ function familiaDeDos(){
     input1.id = "miembro1";
     input1.name = "miembro1";
 
-    const boton1 = document.createElement("input");
-    boton1.type = "submit";
-    boton1.value = "Cargar";
-
-    const resultado8 = document.createElement("div");
-    resultado8.id = "resultado8";
+    const resultadoMiembro1 = document.createElement("div");
+    resultadoMiembro1.id = "resultadoMiembro1";
 
     divMiembro1.appendChild(label1)
     divMiembro1.appendChild(input1)
-    divMiembro1.appendChild(boton1)
-    divMiembro1.appendChild(resultado8)
+    divMiembro1.appendChild(resultadoMiembro1)
 
     const divMiembro2 = document.createElement("div")
 
     const label2 = document.createElement("label");
     label2.for = "miembro2";
-    label2.textContent = "Ingresa el nombre del primer miembro";
+    label2.textContent = "Ingresa el nombre del segundo miembro";
 
     const input2 = document.createElement("input");
     input2.type = "text";
     input2.id = "miembro2";
     input2.name = "miembro2";
 
-    const boton2 = document.createElement("input");
-    boton2.type = "submit";
-    boton2.value = "Cargar";
-
-    const resultado9 = document.createElement("div");
-    resultado9.id = "resultado9";
+    const resultadoMiembro2 = document.createElement("div");
+    resultadoMiembro2.id = "resultadoMiembro2";
 
     divMiembro2.appendChild(label2)
     divMiembro2.appendChild(input2)
-    divMiembro2.appendChild(boton2)
-    divMiembro2.appendChild(resultado9)
+    divMiembro2.appendChild(resultadoMiembro2)
 
-    usuarios[0].nombre = input1.value;
-    usuarios[1].nombre = input2.value;
-    resultado8.textContent = `Miembro 1: ${usuarios[0].nombre}`;
-    resultado9.textContent = `Miembro 2: ${usuarios[1].nombre}`;
+    
+    if(input1.value !="") {
+        resultadoMiembro1.textContent = `Miembro 1: ${usuarios[0].nombre}`;
+    }
+    if(input2.value !="") {
+        resultadoMiembro2.textContent = `Miembro 2: ${usuarios[1].nombre}`;
+    }
+
+    usuarios[0].nombre = nombreMiembro1
+    usuarios[0].valor = 0
+    usuarios[1].nombre = nombreMiembro2
+    usuarios[1].valor = 0
+
+    miembro1.appendChild(divMiembro1);
+    miembro2.appendChild(divMiembro2);
 }
+
 function familiaDeTres(){
     const divMiembro3 = document.createElement("div")
 
@@ -161,20 +161,20 @@ function familiaDeTres(){
     input3.id = "miembro3";
     input3.name = "miembro3";
 
-    const boton3 = document.createElement("input");
-    boton3.type = "submit";
-    boton3.value = "Cargar";
-
-    const resultado10 = document.createElement("div");
-    resultado10.id = "resultado10";
+    const resultadoMiembro3 = document.createElement("div");
+    resultadoMiembro3.id = "resultadoMiembro3";
 
     divMiembro3.appendChild(label3)
     divMiembro3.appendChild(input3)
-    divMiembro3.appendChild(boton3)
-    divMiembro3.appendChild(resultado10)
+    divMiembro3.appendChild(resultadoMiembro3)
 
-    usuarios[2].nombre = input3.value;
-    resultado10.textContent = `Miembro 3: ${usuarios[2].nombre}`;
+    usuarios[2].nombre = nombreMiembro3
+    usuarios[2].valor = 0
+    
+    if(input3.value !="") {
+        resultadoMiembro3.textContent = `Miembro 3: ${usuarios[2].nombre}`;
+    }
+    miembro3.appendChild(divMiembro3);
 }
 function familiaDeCuatro(){
     const divMiembro4 = document.createElement("div")
@@ -188,25 +188,66 @@ function familiaDeCuatro(){
     input4.id = "miembro4";
     input4.name = "miembro4";
 
-    const boton4 = document.createElement("input");
-    boton4.type = "submit";
-    boton4.value = "Cargar";
-
-    const resultado11 = document.createElement("div");
-    resultado11.id = "resultado11";
+    const resultadoMiembro4 = document.createElement("div");
+    resultadoMiembro4.id = "resultadoMiembro4";
 
     divMiembro4.appendChild(label4)
     divMiembro4.appendChild(input4)
-    divMiembro4.appendChild(boton4)
-    divMiembro4.appendChild(resultado11)
+    divMiembro4.appendChild(resultadoMiembro4)
 
     usuarios[3].nombre = input4.value;
-    resultado11.textContent = `Miembro 4: ${usuarios[3].nombre}`;
+    if(input4.value !="") {
+        resultadoMiembro4.textContent = `Miembro 4: ${usuarios[3].nombre}`;
+    }
+    miembro4.appendChild(divMiembro4);
 }
 
-while(dia<=1){
+function cargarNombres() {
+    const botonMiembros = document.createElement("div");
+    const boton = document.createElement("input");
+    boton.type = "submit";
+    boton.id = "nombres"
+    boton.value = "Cargar Miembros";
+
+    botonMiembros.appendChild(boton)
+    botonNombres.appendChild(botonMiembros)
+}
+
+function quienLoHizo() {
+    const divQuienPlatos = document.createElement("div")
+
+    const labelPlatos = document.createElement("label");
+    labelPlatos.for = "quienPlatos";
+    labelPlatos.textContent = "¿Quien lavo los platos hoy?";
+
+    const inputPlatos = document.createElement("input");
+    inputPlatos.type = "text";
+    inputPlatos.id = "quienPlatos";
+    inputPlatos.name = "quienPlatos";
+
+    divQuienPlatos.appendChild(labelPlatos)
+    divQuienPlatos.appendChild(inputPlatos)
+
+    quienHizo.appendChild(divQuienPlatos);
+}
+
+function asignarTareas() {
+    const asignar = document.createElement("div");
+    const botonAsignar = document.createElement("input");
+    botonAsignar.type = "submit";
+    botonAsignar.id = "Asignar"
+    botonAsignar.value = "Asignar Tareas";
+
+    asignar.appendChild(botonAsignar)
+    asignarLasTareas.appendChild(asignar)
+}
+
+
+
+
+
+/* while(dia<=1){
     console.log("Dia "+dia)
-    let lavarPlatos = document.getElementById("platos").value;
     switch(lavarPlatos){
         case usuarios[0].nombre:
             usuarios[0].puntos+=LavarPlatosTarea;
@@ -222,7 +263,7 @@ while(dia<=1){
             break;
         default:
             break;
-    }
+    } */
     /* let limpiarBaño = document.getElementById("baño").value;
     switch(limpiarBaño){
         case usuarios[0].nombre:
@@ -309,7 +350,7 @@ while(dia<=1){
             break;
     } */
 
-    if(habitantes==2){
+/*     if(habitantes==2){
         console.log(usuarios[0].nombre+" tiene "+usuarios[0].puntos+"pts")
         console.log(usuarios[1].nombre+" tiene "+usuarios[1].puntos+"pts")
     }else if(habitantes==3){
@@ -325,7 +366,7 @@ while(dia<=1){
     
 
 dia+=1
-}
+} */
 
 
 
